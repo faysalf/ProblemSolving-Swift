ac909d9e83a765fef70f770e011950fbd0a2bbfd
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-11-23 23:38:08 +0600

    Find the Maximum Achievable Number is solved

---
+++ CompetitiveProgramming/CompetitiveProgramming/Math/easy2769. Find the Maximum Achievable Number.swift
@@ -0,0 +1,14 @@
+//
+//  easy2769. Find the Maximum Achievable Number.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 23/11/23.
+//
+
+import Foundation
+
+//class Solution {
+//    func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+//        return num + t*2
+//    }
+//}
