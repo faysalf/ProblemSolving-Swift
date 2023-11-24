fba809e4c0ac39924ddfb0439fa13907f0b600cd
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-11-25 03:11:44 +0600

    1689 is solved

---
+++ CompetitiveProgramming/CompetitiveProgramming/Greedy/mid-1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.swift
@@ -0,0 +1,16 @@
+//
+//  mid-1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 25/11/23.
+//
+
+//import Foundation
+
+//class Solution {
+//    func minPartitions(_ n: String) -> Int {
+//        guard let mxStr = n.max(), let deciBin = Int(String(mxStr)) else { return 0 }
+//
+//        return deciBin
+//    }
+//}
