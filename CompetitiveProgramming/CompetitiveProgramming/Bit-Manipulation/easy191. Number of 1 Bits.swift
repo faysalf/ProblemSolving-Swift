aa6f005198b336514631a881853b879bc8c8b96c
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-11-30 21:15:42 +0600

    easy 191 bit manipulation is solved

---
+++ CompetitiveProgramming/CompetitiveProgramming/Bit-Manipulation/easy191. Number of 1 Bits.swift
@@ -0,0 +1,23 @@
+//
+//  easy191. Number of 1 Bits.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 30/11/23.
+//
+
+//import Foundation
+//
+//class Solution {
+//    func hammingWeight(_ n: Int) -> Int {
+//        var cnt = 0
+//        var bin = n
+//        
+//        while bin != 0 {
+//            cnt += bin&1
+//            bin >>= 1
+//            
+//        }
+//        
+//        return cnt
+//    }
+//}
