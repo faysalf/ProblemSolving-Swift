41dd014864b921d0a4b75ccccead7a51d9afa183
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-11-30 21:17:54 +0600

    easy 1920 build arr from permutaion is solved

---
+++ CompetitiveProgramming/CompetitiveProgramming/Array/easy1920. Build Array from Permutation.swift
@@ -0,0 +1,22 @@
+//
+//  easy1920. Build Array from Permutation.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 27/11/23.
+//
+
+import Foundation
+
+//class Solution {
+//
+//    func buildArray(_ nums: [Int]) -> [Int] {
+//
+//        var res: [Int] = []
+//        for i in nums {
+//            res.append(nums[i])
+//        }
+//
+//        return res
+//
+//    }
+//}
