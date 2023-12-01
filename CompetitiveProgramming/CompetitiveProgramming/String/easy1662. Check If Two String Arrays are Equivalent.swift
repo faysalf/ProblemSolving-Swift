f1f229007525fec1b2b483a07795ae03ea919d45
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-12-02 02:38:00 +0600

    solved easy 1662

---
+++ CompetitiveProgramming/CompetitiveProgramming/String/easy1662. Check If Two String Arrays are Equivalent.swift
@@ -0,0 +1,30 @@
+//
+//  easy1662. Check If Two String Arrays are Equivalent.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 2/12/23.
+//
+
+//import Foundation
+
+//class Solution {
+//    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+//
+//        let cntW1 = word1.count, cntW2 = word2.count
+//        var conctW1 = "", conctW2 = ""
+//
+//        for i in 0..<max(cntW1, cntW2) {
+//            if i < cntW1 {
+//                conctW1 += word1[i]
+//            }
+//            if i < cntW2 {
+//                conctW2 += word2[i]
+//            }
+//        }
+//
+//        if conctW1 == conctW2 {
+//            return true
+//        }
+//        return false
+//    }
+//}
