3f2f332f4501fc3252fad6be8be513fab8640d81
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-12-06 03:40:40 +0600

    solved math 1688

---
+++ CompetitiveProgramming/CompetitiveProgramming/Math/easy1688. Count of Matches in Tournament.swift
@@ -0,0 +1,14 @@
+//
+//  easy1688. Count of Matches in Tournament.swift
+//  CompetitiveProgramming
+//
+//  Created by Md. Faysal Ahmed on 6/12/23.
+//
+
+import Foundation
+
+class Solution1688 {
+    func numberOfMatches(_ n: Int) -> Int {
+        return n-1
+    }
+}
