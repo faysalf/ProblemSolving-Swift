d72686b7a491f7ea68deab447317f4aba7425b09
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2025-09-23 02:16:50 +0600

    35 - Search insert position - using BS and recursion

--- ProblemSolving-LeetCode/BinarySearch/35-Search-Insert-Position.playground/Contents.swift
+++ ProblemSolving-LeetCode/BinarySearch/35-Search-Insert-Position.playground/Contents.swift
@@ -1,24 +1,49 @@
+//class Solution {
+//    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+//        var idx = 0
+//        var left = 0
+//        var right = nums.count - 1
+//        
+//        while left <= right {
+//            let mid = (left + right) / 2
+//            if nums[mid] == target {
+//                return mid
+//            } else if nums[mid] < target {
+//                left = mid + 1
+//            } else {
+//                right = mid - 1
+//            }
+//        }
+//        
+//        return left
+//    }
+//}
+
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var idx = 0
-        var left = 0
-        var right = nums.count - 1
-        
-        while left <= right {
+        func dfs( _ left: Int, _ right: Int) -> Int {
+            if left > right {
+                return left
+            }
+            
             let mid = (left + right) / 2
             if nums[mid] == target {
                 return mid
+                
             } else if nums[mid] < target {
-                left = mid + 1
+                return dfs(mid+1, right)
+                
             } else {
-                right = mid - 1
+                return dfs(left, mid-1)
             }
+            
         }
         
-        return left
+        return dfs(0, nums.count-1)
     }
 }
 
+
 let obj = Solution()
-let res = obj.searchInsert([1, 3, 5, 6, 9], 4)
+let res = obj.searchInsert([1, 3, 5, 6, 9], 7)
 debugPrint(res)
