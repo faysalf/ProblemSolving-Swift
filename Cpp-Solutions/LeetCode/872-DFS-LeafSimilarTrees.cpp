56a447317f94b30c96e4770464b6adbebf43fb78
faysalf <mdfaysalahmedf@gmail.com>
2025-10-28 18:28:56 +0600

    LeetCode 872 solution - BFS - Leaf Similar Trees

---
+++ Cpp-Solutions/LeetCode/872-DFS-LeafSimilarTrees.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+    public:
+       vector<int> leaf1;
+
+        void dfs(TreeNode* root, bool isFirst) {
+            if (!root) { return; }
+            
+            if (!(root->left && root->right) {
+                if (isFirst) {
+                    leaf1.push_back(root->val);
+                }
+                else {
+                    int current = leaf1.front();
+                    if (current != root->val) {
+                        return;
+                    }
+                    leaf1.erase(leaf1.begin());
+                }
+            }
+
+            if (root->left) {
+                dfs(root->left, isFirst);
+            }
+            if (root->right) {
+                dfs(root->right, isFirst);
+            }
+            
+        };
+        
+        bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        
+            dfs(root1, true);
+            dfs(root2, false);
+        
+            if (leaf1.empty()) {
+                return true;
+            }
+            return false;
+
+        };
+};
